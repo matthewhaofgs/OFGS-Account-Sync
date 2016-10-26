e50778a8d17585cc72057dd45f7a1130194f4f83
matthewhaofgs <matthewha@ofgs.nsw.edu.au>
2016-10-27 09:55:07 +1100

    Init

--- Module1.vb
+++ Module1.vb
@@ -1,7 +1,129 @@
-﻿Module Module1
+﻿Imports System.IO
+Imports System.DirectoryServices
+
+
+Module Main
+
+    Class user
+        Public firstName
+        Public surname
+        Public displayName
+        Public email
+        Public username
+        Public profilePath
+        Public HomePath
+        Public classOf
+        Public employeeID
+        Public employeeNumber
+        Public password
+        Public startDate
+        Public endDate
+    End Class
+
+    Class configSettings
+        Public edumateConnectionString As String
+        Public ldapDirectoryEntry As String
+    End Class
+
 
     Sub Main()
 
     End Sub
+
+
+    Private Function readConfig()
+        Dim config As New configSettings()
+
+        Try
+            ' Open the file using a stream reader.
+            Dim directory As String = My.Application.Info.DirectoryPath
+
+
+
+            Using sr As New StreamReader(directory & "\config.ini")
+                Dim line As String
+                While Not sr.EndOfStream
+                    line = sr.ReadLine
+
+                    Select Case True
+                        Case Left(line, 24) = "edumateConnectionstring="
+                            config.edumateConnectionString = Mid(line, 25)
+                        Case Left(line, 19) = "ldapDirectoryEntry="
+                            config.edumateConnectionString = Mid(line, 20)
+                    End Select
+
+                End While
+
+                readConfig = config
+            End Using
+
+        Catch e As Exception
+            MsgBox(e.Message)
+        End Try
+    End Function
+
+
+
+    Function getEdumateStudents(config As configSettings)
+
+
+        Dim ConnectionString As String = config.edumateConnectionString
+        Dim commandString As String =
+"
+select
+username,
+student_number,
+firstname,
+surname,
+birthdate,
+form_name
+from schoolbox_students
+"
+
+
+        Dim users As New List(Of user)
+
+
+
+        Using conn As New System.Data.Odbc.OdbcConnection(ConnectionString)
+            conn.Open()
+
+            'define the command object to execute
+            Dim command As New System.Data.Odbc.OdbcCommand(commandString, conn)
+            command.Connection = conn
+            command.CommandText = commandString
+
+            Dim dr As System.Data.Odbc.OdbcDataReader
+            dr = command.ExecuteReader
+
+            Dim i As Integer = 0
+            While dr.Read()
+                If Not dr.IsDBNull(0) Then
+                    users.Add(New user)
+
+                    users.Last.password = ""
+                End If
+            End While
+        End Using
+    End Function
+
+
+
+    Sub createUsers(usersToCreate As List(Of user))
+
+    End Sub
+
+    ''' <returns>DirectoryEntry</returns>
+    Public Function GetDirectoryEntry(config As configSettings) As DirectoryEntry
+
+        Dim dirEntry As New DirectoryEntry(config.ldapDirectoryEntry)
+        'Setting username & password to Nothing forces
+        'the connection to use your logon credentials
+        dirEntry.Username = Nothing
+        dirEntry.Password = Nothing
+        'Always use a secure connection
+        dirEntry.AuthenticationType = AuthenticationTypes.Secure
+        Return dirEntry
+    End Function
 
 End Module
